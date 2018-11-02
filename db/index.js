af54ff28ef075c847cd88a77b6e9eabae66e348e
Daniel Kang <danielkang674@gmail.com>
2018-11-01 20:15:28 -0500

    Connect db and todo post route

--- db/index.js
+++ db/index.js
@@ -1,11 +1,16 @@
 const { Client } = require('pg');
-const client = new Client({
+const client = new Client(process.env.PG_URL);
 
-});
+client.connect()
+  .then(() => console.log('db connected'))
+  .catch(err => console.log('db failed to connect', err.stack));
 
-try {
-  await client.connect();
-  console.log('connected to db');
-} catch (err) {
-  console.log('connection error to db', err.stack)
-}
+module.exports.getAllDays = async () => {
+  try {
+    const days = await client.query('SELECT * FROM days');
+    // only need the row data
+    return days.rows;
+  } catch (err) {
+    console.log('DB error getting all days', err.stack);
+  }
+};
--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
+const db = require('./db');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const MODE = process.env.MODE;
+
+if (MODE === 'DEV') {
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+}
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/db/days', async (req, res) => {
+  try {
+    const days = await db.getAllDays();
+    res.json(days);
+  } catch (err) {
+    console.log('Express error getting all days', err.stack);
+  }
+});
+
+app.post('/db/saveTodo', (req, res) => {
+  console.log(req.body);
+  res.end('got it');
+});
 
 app.get('*', (req, res) => {
   res.send('hello');
